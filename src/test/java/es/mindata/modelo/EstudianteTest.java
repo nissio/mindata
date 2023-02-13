64190ecf2dea48768216aa09d55c836b9695c4c4
nissio <nissio@gmail.com>
2023-02-13 15:43:36 -0300

    Se actualiza versión de JUnit, se crean instancias de prueba previo a todos los tests, y actualizan en consecuencia

--- src/test/java/es/mindata/modelo/EstudianteTest.java
+++ src/test/java/es/mindata/modelo/EstudianteTest.java
@@ -1,66 +1,59 @@
 package es.mindata.modelo;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 /**
  * Unit test for Estudiante.
  */
 public class EstudianteTest {
 
-    private String nombre;
-    private Integer edad;
-    private Fecha fechaFinalizacion;
-    private List<Materia> materiasCursadas;
-    private Materia materia1;
-    private Materia materia2;
-    private Estudiante estudiante;
+    private static String nombre;
+    private static Integer edad;
+    private static Fecha fechaFinalizacion;
+    private static List<Materia> materiasCursadas;
+    private static Materia materia1;
+    private static Materia materia2;
+    private static Estudiante estudiante;
 
-    @Before
-    public void before() {
-        this.nombre = "Estudiante A";
-        this.edad = 18;
-        this.fechaFinalizacion = new Fecha(LocalDate.now());
-        this.materiasCursadas = new ArrayList<>();
-        this.materia1 = new Materia("Materia 1", (float) 9.5);
-        this.materiasCursadas.add(materia1);
-        this.materia2 = new Materia("Materia 2", (float) 8);
-        this.materiasCursadas.add(materia2);
+    @BeforeAll
+    public static void beforeAll() {
+        nombre = "Estudiante A";
+        edad = 18;
+        fechaFinalizacion = new Fecha(LocalDate.now());
+        materiasCursadas = new ArrayList<>();
+        materia1 = new Materia("Materia 1", (float) 9.5);
+        materiasCursadas.add(materia1);
+        materia2 = new Materia("Materia 2", (float) 8);
+        materiasCursadas.add(materia2);
         estudiante = Estudiante.builder().nombre(nombre).edad(edad).fechaFinalizacion(fechaFinalizacion)
                 .materiasCursadas(materiasCursadas)
                 .build();
     }
 
     @Test
-    public void nombreOk() {
-        assertEquals(this.estudiante.getNombre(), this.nombre);
+    public void parametrosOk() {
+        assertAll(
+                "Propiedades del estudiante",
+                () -> assertEquals(nombre, estudiante.getNombre()),
+                () -> assertEquals(edad, estudiante.getEdad()),
+                () -> assertEquals(fechaFinalizacion, estudiante.getFechaFinalizacion()),
+                () -> assertEquals(materiasCursadas, estudiante.getMateriasCursadas()));
     }
 
     @Test
-    public void edadOk() {
-        assertEquals(this.estudiante.getEdad(), this.edad);
-    }
-
-    @Test
-    public void fechaFinalizacionOk() {
-        assertEquals(this.estudiante.getFechaFinalizacion(), this.fechaFinalizacion);
-    }
-
-    @Test
-    public void materiasCursadasOk() {
-        assertEquals(this.estudiante.getMateriasCursadas(), this.materiasCursadas);
-    }
-
-    @Test
-    public void materiasCursadasInmutableOk() {
-        this.estudiante.getMateriasCursadas().remove(materia1);
-        assertEquals(this.estudiante.getMateriasCursadas(), this.materiasCursadas);
+    public void modificacionMateriasCursadasInmutableThrowExceptionOk() {
+        assertThrows(UnsupportedOperationException.class, () -> {
+            estudiante.getMateriasCursadas().remove(materia1);
+        });
     }
 
 }
